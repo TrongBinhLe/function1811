b1d5a87646c8a8dabe34a1ef039f6f4472cae46c
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-28 19:47:14 +0700

    filter

---
+++ a.js
@@ -0,0 +1,9 @@
+const arrPerson = [
+    { name: 'Teo', age: 10, height: 140 },
+    { name: 'Ti', age: 15, height: 130 },
+    { name: 'Tun', age: 13, height: 120 },
+];
+
+// arrPerson.forEach(person => console.log(person.height));
+const result = arrPerson.filter(person => person.age > 12);
+console.log(result);
---
+++ b.js
@@ -0,0 +1,5 @@
+const arrPerson = [
+    { name: 'Teo', age: 10, height: 140 },
+    { name: 'Ti', age: 15, height: 130 },
+    { name: 'Tun', age: 13, height: 120 },
+];
